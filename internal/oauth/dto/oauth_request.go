d440ccc16a7b6bb91d49bca24d2732eba5ead830
YungBenn <adisuryo22@gmail.com>
2023-06-26 19:38:01 +0700

    Oauth login and register

--- internal/oauth/dto/oauth_request.go
+++ internal/oauth/dto/oauth_request.go
@@ -1,10 +1,10 @@
 package oauth
 
 type LoginRequestBody struct {
-	Email        string `josn:"email" binding:"email"`
-	Password     string `josn:"password" binding:"required"`
-	ClientID     string `josn:"client_id" binding:"required"`
-	ClientSecret string `josn:"client_secret" binding:"required"`
+	Email        string `json:"email" binding:"email"`
+	Password     string `json:"password" binding:"required"`
+	ClientID     string `json:"client_id" binding:"required"`
+	ClientSecret string `json:"client_secret" binding:"required"`
 }
 
 type RefreshTokenRequestBody struct {
